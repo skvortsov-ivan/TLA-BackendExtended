3ebce6bd72596f476d7f36c4f7eb67f0375efc19
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2026-03-30 15:09:06 +0200

    Add project files.

---
+++ TLA-BackendExtended/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
