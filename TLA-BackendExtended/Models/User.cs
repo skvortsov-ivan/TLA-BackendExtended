8615b1159409fd3cc16c011f9f06a07d9bd61ef1
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2026-03-31 15:48:37 +0200

    Quick commit push for participants to have the same Nuget packages as work goes on.

--- TLA-BackendExtended/Models/User.cs
+++ TLA-BackendExtended/Models/User.cs
@@ -3,10 +3,16 @@
     public class User
     {
         public int Id { get; set; }
-        public string Userame { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public int Age { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Location { get; set; }
+
+        public bool DarkMode { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string InternalAdminNote { get; set; } = "Hemlig data";
+
     }
 }
--- TLA-BackendExtended/Services/IUserService.cs
+++ TLA-BackendExtended/Services/IUserService.cs
@@ -1,10 +1,14 @@
-﻿namespace TLA_BackendExtended.Services
+﻿using TLA_BackendExtended.Models;
+
+namespace TLA_BackendExtended.Services
 {
     public interface IUserService
     {
         Task<User> CreateUserAsync(string username, string password, int age, int weight, string location);
-        Task<User?> GetUserAsync(string username);
-        Task<User?> UpdateUserAsync(string username, string password, int age, int weight, string location);
-        Task<User?> UpdateColourModeAsync(string username, bool colourMode);
+        Task<User> GetUserAsync(string username);
+        Task<User> UpdateUserAsync(string username, string password, int age, int weight, string location);
+        Task<User> UpdateColourModeAsync(string username, bool colourMode);
+        Task DeleteUserAsync(string username);
+        Task<List<User>> GetAllUsersAsync();
     }
 }
